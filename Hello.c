49a2a977b05ff1eebf28abe0a2a4069459789c3e
SBP-Learner <sbpd206@gmail.com>
2025-07-05 13:10:44 +0530

    Page_and_C-Code

---
+++ Hello.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // Code for vote eligibility check
+    /*int a;
+    printf("Enter your age : ");
+    scanf("%d", &a);
+    if (a > 18) {
+        printf("You are eligible to vote.\n");
+    } else if (a > 0 && a < 18) {
+        printf("You are not eligible to vote.\n");
+    } else {
+        printf("Invalid age.");
+    }  */
+
+    // Code for prime number identifier (Non-continous)
+    // for continous we use do while loop
+
+    /* int a,i,prime = 1;
+    printf("Enter a number : ");
+    scanf("%d", &a);
+    if (a <= 1) {
+        printf("Nor prime neither composite.\n");
+    } else {
+        for (i = 2;i*i <= a; i++) {
+            if (a%i == 0) {
+                prime = 0;
+                break;
+            }
+        }
+    } 
+    if (prime) {
+    printf("It is a prime number.\n");
+    }
+    else {
+        printf("It is a Composite number.\n");
+    } */
+
+    
+    // Simple calculator
+
+
+    float a,b,operation;
+    char choice;
+    char op;
+    printf("------Simple Calculator-------\n");
+    do {
+        printf("Enter first number : ");
+        scanf("%f", &a);
+        printf("Enter the operator(+,-,*,/,^) : \n");
+        scanf(" %c", &choice);
+        printf("Enter second number : \n");
+        scanf("%f", &b);
+        
+        switch(choice) {
+            case '+' :
+            operation = a + b;
+            printf("%.2f\n",operation);
+            break;
+            case '-' :
+            operation = a - b;
+            printf("%.2f\n",operation);
+            break;
+            case '*' :
+            operation = a * b;
+            printf("%.2f\n",operation);
+            break;
+            case '/' :
+            if (b == 0){
+                printf("Second number cannot be 0.\n");
+            } else {
+            operation = a / b;
+            printf("%.2f\n",operation);
+                }
+            break;
+            case '^' :
+            operation = pow(a,b);
+            printf("%.2f ^ %.2f = %.2f\n",a,b,operation);
+            break;
+            default :
+            printf("Invalid operator\n");
+        }
+        printf("Do you want to continue(Y/N) : ");
+        scanf(" %c",&op);
+    } while (op == 'Y' || op == 'y');
+    printf("Thank you!\n");
+    return 0;
+}
