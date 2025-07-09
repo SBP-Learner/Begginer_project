1ad0c620ef4c85bcbe4040edc22b917ed74c6d0d
SBP-Learner <sbpd206@gmail.com>
2025-07-09 20:49:31 +0530

    Explanation of code_0

--- Hello.c
+++ Hello.c
@@ -4,11 +4,13 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define True = 1;
-#define False = 0;
+#define True 1
+#define False 0
 
 // enum function
-/* enum Cards {
+/* enum Cards { // These are the tags created using enums. 
+                   Theit values can't be changed after assigning. 
+                   They only store integers.
     club = 1,
     diamonds = 10,
     hearts = 20,
@@ -19,10 +21,10 @@
 
 int main() {
     // Code for vote eligibility check
-    /*int a;
-    printf("Enter your age : ");
-    scanf("%d", &a);
-    if (a > 18) {
+    /*int a; // Declares a variable
+    printf("Enter your age : "); // Directs the user for specific input
+    scanf("%d", &a); // Takes a specific input from user
+    if (a > 18) { // Checks the condition if true - executes, if false goes to else 
         printf("You are eligible to vote.\n");
     } else if (a > 0 && a < 18) {
         printf("You are not eligible to vote.\n");
@@ -36,21 +38,21 @@ int main() {
     /* int a,i,prime = 1;
     printf("Enter a number : ");
     scanf("%d", &a);
-    if (a <= 1) {
+    if (a <= 1) { //Excludes all other numbers which is not Natural. 
         printf("Nor prime neither composite.\n");
-    } else {
+    } else { // Creates a loop from 2 to the given number
         for (i = 2;i*i <= a; i++) {
-            if (a%i == 0) {
-                prime = 0;
-                break;
+            if (a%i == 0) { // Checks if the number is composite
+                prime = 0; // Assigns a false value for prime variable
+                break; // breaks the loop if the given number gives 0 as reminder by dividing it by the numbers between 2-a
             }
         }
     } 
-    if (prime) {
-    printf("It is a prime number.\n");
+    if (prime) { // After exiting the loop it checks if the prime variable still has 1 
+    printf("It is a prime number.\n"); // Displays that number is prime
     }
-    else {
-        printf("It is a Composite number.\n");
+    else { // if the number becomes 0 by the loop 
+        printf("It is a Composite number.\n"); // Dosplays its Composite
     } */
 
     
