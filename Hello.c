0d7c1371ce73987e4cc21bb5956182f2daf616f8
SBP-Learner <sbpd206@gmail.com>
2025-07-11 20:42:56 +0530

    Minor Fixes

--- Hello.c
+++ Hello.c
@@ -115,20 +115,21 @@ int main() {
 	printf("Size of enum variable = %d bytes", sizeof(card));
     printf("Enum variable = %d", (card); */	
 
-    // Number guessing game (Continous loop)
-    /* printf("____Welcome to____\n");
+    // Number guessing game (Continous loop) Everytime the number changes
+   /*  printf("____Welcome to____\n");
     printf("____Number guessing game____\n");
+    printf("Warning : This game changes the random number every time you make a guess.\n");
+    printf("Game mode : Extreme difficulty(Win on luck)\n");
     printf("\n");
     
-   int a;
+    int a;
     int min = 0, max = 9;
-  int random;
-  srand(time(NULL));
-  
-   do {
+    int random, correct = 0, played = 0;
+    srand(time(NULL));
     
+    random = rand() % (max - min + 1) + min; // Generate random number once
     
-    
+   do { 
     printf("Guess a number from (0-9)\n");
     if (scanf("%d", &a) != 1) {
         printf("Invalid input.\nPlease enter a number between 0 to 9.\n\n");
@@ -141,19 +142,30 @@ int main() {
         random = rand() % (max - min + 1) + min;
     if (a == random) {
         printf("\nHurray!\n");
+        correct++;
     } else {
         printf("\nOops!!\n");
     }
     printf("You guessed %d.\nThe number was %d\n",a, random);
+    played++;
 } else {
     printf("Invalid number.\nTry again.\n\n");
 }
   } while (random != a);
 
-  printf("Congratulations! You guesses the right number.\n\n"); */
+  printf("Congratulations! You guesses the right number.\n\n");
+   printf("\nThank you for playing.\n");
+  printf("You took %d tries.\n", played);
+  printf("You guessed %d correct.\n", correct);
+  printf("Your score is %d/%d\n",correct,played);
+
+  if (played > 0) {
+  printf("Your win rate is %.2f%%\n", ((float)correct * 100) / played);
+  } */
    
+  
   // Number guessing game (Optional loop)
-   printf("____Welcome to____\n");
+/*    printf("____Welcome to____\n");
     printf("____Number guessing game____\n");
     printf("\n");
     
@@ -200,7 +212,10 @@ int main() {
 
   if (played > 0) {
   printf("Your win rate is %.2f%%\n",((float)correct * 100) / played);
-}
+} */
+
+
+    // ATM Machines
     return 0;
 }
 
