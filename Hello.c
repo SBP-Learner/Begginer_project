ab26400663f3e12fe9939469b16dd00d2e1eeecf
SBP-Learner <sbpd206@gmail.com>
2025-07-14 18:18:32 +0530

    Slight improvement

--- Hello.c
+++ Hello.c
@@ -216,6 +216,31 @@ int main() {
 
 
     // ATM Machines
+     printf("Welcome to ATM Machine\n");
+     int PIN = 9999,EPin,choose,
+         balance = 10000,withdraw,deposit,Exit = 0;
+    
+      
+     printf("Select what you want to do\n");
+        printf("1. Withdraw\n");
+        printf("2. Deposit\n");
+        printf("3. Check Balance\n");
+        printf("4. Exit\n");
+
+       switch(withdraw) {
+        printf("Enter your PIN : ");
+        scanf("%d", &EPin);
+        if (EPin == PIN) {
+            printf("")
+
+        }
+
+
+       } 
+       
+
+
+
     return 0;
 }
 
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello there!")
+# First Programm
+print("_________Welcome________\n")
+print("Odd Even Identifier\n")
+num = int(input("Enter a number : "))
+if num % 2 == 0 :
+    print(f"{num} is an even number.\n")
+else :
+    print(f"{num} is an odd number.")
