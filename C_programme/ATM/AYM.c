85ebf61d375e25749ab4b342c68779ef1e1456e3
SBP-Learner <sbpd206@gmail.com>
2025-07-17 21:43:06 +0530

    File Organising

--- C_programme/ATM/AYM.c
+++ C_programme/ATM/AYM.c
@@ -16,14 +16,18 @@
         printf("2. Deposit\n");
         printf("3. Check Balance\n");
         printf("4. Exit\n");
-
+        
+        if (scanf("%d", choose)) {
        switch(withdraw) {
-        case :
+        case 1 :
         printf("Enter your PIN : ");
         scanf("%d", &EPin);
         if (EPin == PIN) {
-            printf("")
+            printf("");
+            }
+            break;
         default :
+         printf("");
         }
         return 0;
     }
