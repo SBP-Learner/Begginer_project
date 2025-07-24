e096bf9140f80dccc5d40056656f897da5bb8a90
SBP-Learner <sbpd206@gmail.com>
2025-07-24 17:56:20 +0530

    PY calc fix

--- Python/calculator.py
+++ Python/calculator.py
@@ -4,29 +4,29 @@ while True :
   num1 = float(input("Enter the first number : "))
   operator = input("Enter the operator (+,-,*,/,^): ")
   num2 = float(input("Enter the second number : "))
- except :
-  print("Invalid input.")
-
- if operator == '+' :
-  sum = num1+num2
-  print(f"Sum of {num1} and {num2} is {sum}") 
- elif operator == '-':
-  sub = num1-num2
-  print(f"Substraction of {num2} from {num1} is {sub}")
- elif operator == '*' :
-  mult = num1*num2
-  print(f"Product of {num1} and {num2} is {mult}")
- elif operator == '/' :
-  if num2 == 0 :
-    print(f"Denominator number cannot be zero.")
+ 
+  if operator == '+' :
+   sum = num1+num2
+   print(f"Sum of {num1} and {num2} is {sum}") 
+  elif operator == '-':
+   sub = num1-num2
+   print(f"Substraction of {num2} from {num1} is {sub}")
+  elif operator == '*' :
+   mult = num1*num2
+   print(f"Product of {num1} and {num2} is {mult}")
+  elif operator == '/' :
+   if num2 == 0 :
+     print(f"Denominator number cannot be zero.")
+   else :
+     div = num1//num2
+     print(f"{num1} divided by {num2} is {div}")
+  elif operator == '^' :
+   pow = num1**num2
+   print(f"{num1} to power {num2} is {pow}")
   else :
-    div = num1//num2
-    print(f"{num1} divided by {num2} is {div}")
- elif operator == '^' :
-  pow = num1**num2
-  print(f"{num1} to power {num2} is {pow}")
- else :
-  print("invalid operator.")
+   print("invalid operator.")
+ except :
+   ("Invalid input.")
 
  choose = input("Do you want to continue?\nType Yes to continue : ")
  if choose != "Yes" and choose != "yes" and choose != "YES" :
